fa817f1886f90be4e9bac9dfcef13feeb1c758c5
OtakuCaptain <chen_t_y@hotmail.com>
2017-01-17 11:11:40 +0800

    tablayout

--- app/src/main/java/com/chen/smartapplication/MainActivity.java
+++ app/src/main/java/com/chen/smartapplication/MainActivity.java
@@ -2,9 +2,15 @@ package com.chen.smartapplication;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.chen.smartapplication.fragment.ButlerFragment;
+import com.chen.smartapplication.fragment.GirlFragment;
+import com.chen.smartapplication.fragment.UserFragment;
+import com.chen.smartapplication.fragment.WeChatFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+
         initData();
         initView();
     }
@@ -31,12 +40,38 @@ public class MainActivity extends AppCompatActivity {
         mTitle.add("美女社区");
         mTitle.add("个人中心");
 
-        mFragment=new ArrayList<>();
+        mFragment = new ArrayList<>();
+        mFragment.add(new ButlerFragment());
+        mFragment.add(new WeChatFragment());
+        mFragment.add(new GirlFragment());
+        mFragment.add(new UserFragment());
 
     }
 
     private void initView() {
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
 
+        mViewPager.setOffscreenPageLimit(mFragment.size());
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragment.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragment.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitle.get(position);
+            }
+        });
+
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
 
